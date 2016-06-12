5d4007158fc1ec1c8217931c2268f75f74b4e281
Rahul Kadyan <hi@znck.me>
2016-06-12 13:36:30 +0530

    Add google analytics

--- content/_includes/base.blade.php
+++ content/_includes/base.blade.php
@@ -30,10 +30,22 @@
     <meta name="msapplication-config" content="/assets/icons/browserconfig.xml?v=allb2kW4WL">
     <meta name="theme-color" content="#344952">
 
+    <link rel="author" href="/humans.txt" />
+
     <link rel="stylesheet" href="@url('/assets/styles/style.css')">
 </head>
 
 <body>
     @yield('body')
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-79188466-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </body>
 </html>
