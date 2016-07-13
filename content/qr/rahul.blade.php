0f8974489fe9550ace1ae1338db0193b0dfdbc78
Rahul Kadyan <hi@znck.me>
2016-07-13 23:00:05 +0530

    Add QR profile

--- content/qr/rahul.blade.php
+++ content/qr/rahul.blade.php
@@ -3,9 +3,9 @@
 @section('contact')
     <div class="container-fluid p-y-3 vcard">
         <div class="text-xs-center">
-            <img src="/assets/images/qr/suwardhan.jpg" alt="Suwardhan Ahirrao" class="profile">
+            <img src="/assets/images/qr/rahul.jpg" alt="Rahul Kadyan" class="profile">
         </div>
-        <h4 class="text-xs-center m-t-2 fn">Suwardhan Ahirrao</h4>
+        <h4 class="text-xs-center m-t-2 fn">Rahul Kadyan</h4>
         <div class="text-xs-center">Operations</div>
         <br>
         <div class="card">
@@ -18,20 +18,24 @@
         <div class="card">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item p-y-1">
-                    <i class="fa fa-fw fa-phone"></i> <a href="tel:+918473994808" class="text-link tel">+91-84-73-994808</a>
+                    <i class="fa fa-fw fa-phone"></i> <a href="tel:+918473994943" class="text-link tel">+91-84-73-994943</a>
                 </li>
                 <li class="list-group-item p-y-1">
-                    <i class="fa fa-fw fa-envelope"></i> <a href="mailto:suwardhan@scalex.xyz" class="text-link email">suwardhan@scalex.xyz</a>
+                    <i class="fa fa-fw fa-envelope"></i> <a href="mailto:rahul@scalex.xyz" class="text-link email">rahul@scalex.xyz</a>
                 </li>
                 <li class="list-group-item p-y-1">
                     <i class="fa fa-fw fa-link"></i> <a href="http://scalex.xyz" class="text-link url">http://scalex.xyz</a>
+                </li>
+                <li class="list-group-item p-y-1">
+                    <i class="fa fa-fw fa-link"></i> <a href="http://znck.me" class="text-link url">http://znck.me</a>
                 </li>
             </ul>
         </div>
         <div class="card">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item p-y-1">
-                    <i class="fa fa-fw fa-graduation-cap"></i> Bachelor of Design (IIT Guwahati)
+                    <i class="fa fa-fw fa-graduation-cap"></i>
+                    B. Tech. (IIT Guwahati)
                 </li>
             </ul>
         </div>
